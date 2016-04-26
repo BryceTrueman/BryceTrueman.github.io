55a2213f0f89862be19d21ca7c9d1246d437756b
Bryce Trueman <truemanbryce@gmail.com>
2016-04-26 13:07:08 -0600

    Update smoothscroll.js

--- smoothscroll.js
+++ smoothscroll.js
@@ -35,3 +35,89 @@ a=b.extend({},c.defaults,a);return a.lazy?this.bind(a.event,function(d){var c=b(
 jQuery(function($){ $.localScroll({filter:'.smoothScroll'}); });
 
 
+/*
+Count down until any date script-
+By JavaScript Kit (www.javascriptkit.com)
+Over 200+ free scripts here!
+Modified by Robert M. Kuhnhenn, D.O. 
+on 5/30/2006 to count down to a specific date AND time,
+on 10/20/2007 to a new format, and 1/10/2010 to include
+time zone offset.
+*/
+
+var current="Race is Here!";    //-->enter what you want the script to display when the target date and time are reached, limit to 20 characters
+var year=2016;    //-->Enter the count down target date YEAR
+var month=5;      //-->Enter the count down target date MONTH
+var day=28;       //-->Enter the count down target date DAY
+var hour=8;      //-->Enter the count down target date HOUR (24 hour clock)
+var minute=00;    //-->Enter the count down target date MINUTE
+var tz=-8;        //-->Offset for your timezone in hours from UTC (see http://wwp.greenwichmeantime.com/index.htm to find the timezone offset for your location)
+
+//    DO NOT CHANGE THE CODE BELOW!
+var montharray=new Array("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+
+function countdown(yr,m,d,hr,min){
+    theyear=yr;themonth=m;theday=d;thehour=hr;theminute=min;
+    var today=new Date();
+    var todayy=today.getYear();
+    if (todayy < 1000) {todayy+=1900;}
+    var todaym=today.getMonth();
+    var todayd=today.getDate();
+    var todayh=today.getHours();
+    var todaymin=today.getMinutes();
+    var todaysec=today.getSeconds();
+    var todaystring1=montharray[todaym]+" "+todayd+", "+todayy+" "+todayh+":"+todaymin+":"+todaysec;
+    var todaystring=Date.parse(todaystring1)+(tz*1000*60*60);
+    var futurestring1=(montharray[m-1]+" "+d+", "+yr+" "+hr+":"+min);
+    var futurestring=Date.parse(futurestring1)-(today.getTimezoneOffset()*(1000*60));
+    var dd=futurestring-todaystring;
+    var dday=Math.floor(dd/(60*60*1000*24)*1);
+    var dhour=Math.floor((dd%(60*60*1000*24))/(60*60*1000)*1);
+    var dmin=Math.floor(((dd%(60*60*1000*24))%(60*60*1000))/(60*1000)*1);
+    var dsec=Math.floor((((dd%(60*60*1000*24))%(60*60*1000))%(60*1000))/1000*1);
+    if(dday<=0&&dhour<=0&&dmin<=0&&dsec<=0){
+        document.getElementById('count2').innerHTML=current;
+        document.getElementById('count2').style.display="inline";
+        document.getElementById('count2').style.width="390px";
+        document.getElementById('dday').style.display="none";
+        document.getElementById('dhour').style.display="none";
+        document.getElementById('dmin').style.display="none";
+        document.getElementById('dsec').style.display="none";
+        document.getElementById('days').style.display="none";
+        document.getElementById('hours').style.display="none";
+        document.getElementById('minutes').style.display="none";
+        document.getElementById('seconds').style.display="none";
+        document.getElementById('spacer1').style.display="none";
+        document.getElementById('spacer2').style.display="none";
+        return;
+    }
+    else {
+        document.getElementById('count2').style.display="none";
+        document.getElementById('dday').innerHTML=dday;
+        document.getElementById('dhour').innerHTML=dhour;
+        document.getElementById('dmin').innerHTML=dmin;
+        document.getElementById('dsec').innerHTML=dsec;
+        setTimeout("countdown(theyear,themonth,theday,thehour,theminute)",1000);
+    }
+}
+
+
+$(document).ready(function() {
+    $('.maps').click(function () {
+        $('.maps iframe').css("pointer-events", "auto");
+    });
+    
+    $( ".maps" ).mouseleave(function() {
+      $('.maps iframe').css("pointer-events", "none"); 
+    });
+ });  
+
+
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-75413919-1', 'auto');
+  ga('send', 'pageview');
